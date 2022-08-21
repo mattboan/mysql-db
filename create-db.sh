1514066286897ea68177378c38dd5befdcf6951a
mattboan <matt.n.boan@gmail.com>
2022-08-21 17:43:17 +0800

    Fixed up some of the default fields

--- create-db.sh
+++ create-db.sh
@@ -14,8 +14,11 @@ read -sp "Database root password: " db_root_password
 # Create the docker network
 docker network create $db_network
 
+# Creat the docker volume
 docker volume create $db_volume
 
-docker run -v potholes-db-volume:/var/lib/mysql --network=potholes-network -p 3306:3306 --detach --env MARIA_DATABASE=$db_name --env MARIA_USER=$db_user_name --env MARIADB_PASSWORD=$db_password --env MARIADB_ROOT_PASSWORD=$db_root_password --name potholes-db mariadb:latest
+# Create and run the new docker image
+docker run -v $db_volume:/var/lib/mysql --network=db_network -p 3306:3306 --detach --env MARIA_DATABASE=$db_name --env MARIA_USER=$db_user_name --env MARIADB_PASSWORD=$db_password --env MARIADB_ROOT_PASSWORD=$db_root_password --name $db_name mariadb:latest
 
+# Exit
 echo 'Done!'
